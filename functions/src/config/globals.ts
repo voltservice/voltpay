491a371bf5b5806266acdf5df838d61b1bad01b8
MetalHuman38 <106476905+MetalHuman38@users.noreply.github.com>
2025-10-14 10:38:41 +0100

    WIF-TEST

--- functions/src/config/globals.ts
+++
@@ -1,8 +0,0 @@
-// // src/config/globals.ts
-// import { setGlobalOptions } from "firebase-functions";
-
-// setGlobalOptions({
-//   region: "europe-west2",
-//   maxInstances: 10,
-//   enforceAppCheck: true,  // <- default ON for all HTTPS (onRequest/onCall)
-// });
--- functions/src/http/routes/health.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const health = (req: Request, res: Response) => {
-  res.status(200).send("OK");
-};
